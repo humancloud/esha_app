5e0c718de1e5739e9759762836c7301dba0d054c
yogeshHumancloud <yogesh.vitekar@humancloud.co.in>
2025-09-16 12:40:04 +0530

    app build fixes

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,3 +1,9 @@
+import java.util.Properties
+import java.io.FileInputStream
+
+// Toggle this for local dev vs production
+val useDebugKeysForRelease = false
+
 plugins {
     id("com.android.application")
     id("kotlin-android")
@@ -5,8 +11,14 @@ plugins {
     id("dev.flutter.flutter-gradle-plugin")
 }
 
+val keystoreProperties = Properties()
+val keystorePropertiesFile = rootProject.file("key.properties")
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
+}
+
 android {
-    namespace = "com.example.myapp"
+    namespace = "com.humancloud.ishaai"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -17,10 +29,19 @@ android {
 
     kotlinOptions { jvmTarget = JavaVersion.VERSION_11.toString() }
 
+    signingConfigs {
+        create("release") {
+            keyAlias = keystoreProperties["keyAlias"] as String
+            keyPassword = keystoreProperties["keyPassword"] as String
+            storeFile = keystoreProperties["storeFile"]?.let { File(it as String) }
+            storePassword = keystoreProperties["storePassword"] as String
+        }
+    }
+
     defaultConfig {
         // TODO: Specify your own unique Application ID
         // (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.myapp"
+        applicationId = "com.humancloud.ishaai"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
@@ -31,19 +52,11 @@ android {
 
     buildTypes {
         release {
-            // TODO: Add your own signing config for the release build.
-            // Signing with the debug keys for now, so `flutter run --release` works.
-            signingConfig = signingConfigs.getByName("debug")
-        }
-    }
-}
-
-flutter { source = "../.." }
-    }
-
-    buildTypes {
-        release {
-            signingConfig = signingConfigs.getByName("release")
+            signingConfig = if (useDebugKeysForRelease) {
+                signingConfigs.getByName("debug")
+            } else {
+                signingConfigs.getByName("release")
+            }
         }
     }
 }
